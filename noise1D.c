99c9bd6c78fce844ca259649f849d9e4a3810279
dcdnce <eric.cantelpi@gmail.com>
2022-06-09 13:22:23 -0400

    fractal sum 1D

--- noise1D.c
+++ noise1D.c
@@ -14,20 +14,18 @@ static inline void	valueNoise1D(long seed) {
 static inline float	eval(const float x) {
 	int	xMin = (int)x;
 	int	xMax = xMin + 1;
-	//xMax &= D_kMaxVerticesMask;
-	if (xMin == D_kMaxVertices)
-		xMax = 0;
+	xMax &= D_kMaxVerticesMask;
+	//if (xMin == D_kMaxVertices)
+	//	xMax = 0;
 	float	t = x - xMin;
 	t = (1 - cos(t * M_PI)) * 0.5; // cos curve
 	return (lerp(g_n.r[xMin], g_n.r[xMax], t));
 }
 
 void	noise1D(void) {
-	valueNoise1D(2011);
+	valueNoise1D(7);
 
-	const int	amplitude = 50;
-	const float	frequency = 0.5f;
-	const int	numSteps = W / 4;
+	const int	numSteps = W;
 
 	t_vec2f		a = {0};
 	t_vec2f		v = {0};
@@ -35,19 +33,25 @@ void	noise1D(void) {
 	int			color = ft_create_trgb(0, 50, 100, 255);
 
 	for (int i = 0 ; i < numSteps; ++i) {
-		a.x = ((float)i / ((float)numSteps - 1) * D_kMaxVertices);
-		a.y = eval(a.x * frequency);
+		a.x = (float)i / ((float)numSteps - 1) * D_kMaxVertices;
+
+		float	scale = 100.0f;
+		float	frequency = 0.03125f;
+
+		const int numLayers = 5;
+		a.y = 0;
+		for (int j = 0 ; j < numLayers ; j++) {
+			a.y += eval(a.x * frequency) * scale;
+			frequency *= 2;
+			scale *= 0.5f;
+		}
 
 		v_old = v;
-
 		/* V.X RENDERING */
-		v.x = a.x;
-		
+		v.x = i * (W / numSteps);  //fullscreen
 		/* V.Y RENDERING */
-		v.y = (H + amplitude) / 2 - a.y * amplitude;
-
+		v.y = H / 2 - a.y;
 		if (v_old.x + v_old.y != 0)
 			dda(v_old, v, 2, color);
-		//pixelPut(&g_n.img, v.x, v.y, color, 10);
 	}
 }
